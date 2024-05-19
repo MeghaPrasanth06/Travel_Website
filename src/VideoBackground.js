d6b835889e49c0f32790ea9929e5447c3492a751
annatresasiji <143536952+annatresasiji@users.noreply.github.com>
2024-05-19 22:37:39 +0530

    Create VideoBackground.js

---
+++ src/VideoBackground.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function VideoBackground() {
+  return (
+    <video src="https://cdn.pixabay.com/video/2023/01/15/146633-789534286_tiny.mp4" autoPlay loop muted></video>
+  );
+}
+
+export default VideoBackground;
