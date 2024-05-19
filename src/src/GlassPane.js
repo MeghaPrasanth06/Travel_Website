5badd18ff824b6f6435a869dccca933274a29597
annatresasiji <143536952+annatresasiji@users.noreply.github.com>
2024-05-19 22:38:51 +0530

    Create GlassPane.js

---
+++ src/src/GlassPane.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function GlassPane() {
+  return (
+    <div className="glass">
+      <h1>WanderLust India</h1>
+      <p>Explore the beauty of India with us</p>
+      <button id="but" onClick={explore}>Explore</button>
+    </div>
+  );
+}
+
+export default GlassPane;
